6c479beddf83ba0231f836b9721af850201b9f88
Seth Lachman <lachmanseth17@gmail.com>
2019-06-16 11:20:13 -0400

    Max recurring solution.

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -3,12 +3,43 @@ Given a string of text, find and return the most recurring character.
 e.g maxRecurringChar('aabacada') // will return 'a'
 */
 
-
-
 function maxRecurringChar(text) {
-    // Code goes here
+  const charStore = {};
+  let maxChar = "";
+  let maxCharValue = 0;
+  for (let i = 0; i < text.length; i++) {
+    charStore[text[i]] = charStore[text[i]] + 1 || 1;
+  }
+  for (let count in charStore) {
+    if (charStore[count] > maxCharValue) {
+      maxChar = count;
+      maxCharValue = charStore[count];
+    }
+  }
+  return maxChar;
 }
 
+// function maxRecurringChar(text) {
+//   let charMap = {}
+//   let maxCharValue = 0
+//   let maxChar = ''
 
+//   for (let char of text) {
+//     if (charMap.hasOwnProperty(char)) {
+//       charMap[char]++
+//     } else {
+//       charMap[char] = 1
+//     }
+//   }
+
+//   for (let char in charMap) {
+//     if (charMap[char] > maxCharValue) {
+//       maxCharValue = charMap[char]
+//       maxChar = char
+//     }
+//   }
+
+//   return maxChar
+// }
 
 module.exports = maxRecurringChar;
