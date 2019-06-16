89c03808bacc3e712e88110316065516e2e997a5
Seth Lachman <lachmanseth17@gmail.com>
2019-06-16 13:10:44 -0400

    Completed cap sentence challenge.

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -4,13 +4,36 @@ return the equivalent of the sentence when capitalised. E.g
   capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
 */
 
-
-
-
 function capSentence(text) {
-   // Code goes here
+  const splitText = text.toLowerCase().split(" ");
+  const caps = splitText.map(word => {
+    return word.substring(0, 1).toUpperCase() + word.substring(1);
+  });
+
+  return caps.join(" ");
 }
 
+// forEach Method
+// function capSentence(text) {
+//   let wordsArray = text.toLowerCase().split(' ')
+//   let capsArray = []
 
+//   wordsArray.forEach(word => {
+//     capsArray.push(word[0].toUpperCase() + word.slice(1))
+//   });
 
-module.exports = capSentence
+//   return capsArray.join(' ')
+// }
+
+// Replace Method
+// function capSentence(text) {
+//   let wordsArray = text.toLowerCase().split(" ");
+
+//   let capsArray = wordsArray.map(word => {
+//     return word.replace(word[0], word[0].toUpperCase());
+//   });
+
+//   return capsArray.join(" ");
+// }
+
+module.exports = capSentence;
