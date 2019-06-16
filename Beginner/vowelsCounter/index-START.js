87d456ac8da109139b510175389837dc169d66fa
Seth Lachman <lachmanseth17@gmail.com>
2019-06-16 10:46:04 -0400

    Added vowel counter solutions.

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,11 +3,27 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
+// Iterative solution
+// function vowelsCounter(text) {
+//   let count = 0;
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   text = text.toLowerCase();
+//   for (let i = 0; i < text.length; i++) {
+//     if (vowels.includes(text[i])) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
 
+// Regex solution
 function vowelsCounter(text) {
-    // Code goes here
+  let matchingChars = text.match(/[aeiou]/gi);
+  if (matchingChars) {
+    return matchingChars.length;
+  } else {
+    return 0;
+  }
 }
-
-
 
 module.exports = vowelsCounter;
