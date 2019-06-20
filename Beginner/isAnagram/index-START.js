1523b65aebd21751f72e99793beb93521c744586
Seth Lachman <lachmanseth17@gmail.com>
2019-06-20 08:49:04 -0400

    Completed anagram challenge.

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -5,11 +5,58 @@ don't. E.g
     isAnagram('silent', 'listen') // should return true
 */
 
+// function isAnagram(stringA, stringB) {
+//   const firstString = stringA.toLowerCase().split("");
+//   const secondString = stringB.toLowerCase().split("");
 
+//   if (firstString.length !== secondString.length) {
+//     return false;
+//   }
+
+//   return firstString.every(char => {
+//     return secondString.includes(char);
+//   });
+// }
+
+// function isAnagram(stringA, stringB) {
+//   const sanitizeString = function(str) {
+//     return str
+//       .toLowerCase()
+//       .replace(/[^a-z\d]/g, "")
+//       .split("")
+//       .sort()
+//       .join("");
+//   };
+//   return sanitizeString(stringA) == sanitizeString(stringB);
+// }
 
 function isAnagram(stringA, stringB) {
-    // Code goes here
+  function createCharMap(text) {
+    let charMap = {};
+
+    for (let char of text) {
+      if (charMap.hasOwnProperty(char)) {
+        charMap[char]++;
+      } else {
+        charMap[char] = 1;
+      }
+    }
+
+    return charMap;
+  }
+
+  if (stringA.length === stringB.length) {
+    let stringAMap = createCharMap(stringA);
+    let stringBMap = createCharMap(stringB);
+    for (let char in stringAMap) {
+      if (stringAMap[char] !== stringBMap[char]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
 }
 
-
-module.exports = isAnagram
+module.exports = isAnagram;
