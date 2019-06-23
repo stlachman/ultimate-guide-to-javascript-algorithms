f46113865a220ed5f45da61bf97f2df1bf0aa918
Seth Lachman <lachmanseth17@gmail.com>
2019-06-23 10:30:27 -0400

    Completed array chunking exercise.

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -4,9 +4,60 @@
  E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
-function chunkArray(array, size) {
-    // Code goes here
+function chunkArray(arr, size) {
+  let result = [];
+  let current = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (result[current] == undefined) {
+      result.push([arr[i]]);
+    } else if (result[current].length === size) {
+      result.push([arr[i]]);
+      current++;
+    } else {
+      result[current].push(arr[i]);
+    }
+  }
+  return result;
 }
 
+// function chunkArray(array, size) {
+//   let result = [];
+//   for (value of array) {
+//     let lastArray = result[result.length - 1];
+//     if (!lastArray || lastArray.length == size) {
+//       result.push([value]);
+//     } else {
+//       lastArray.push(value);
+//     }
+//   }
+//   return result;
+// }
 
-module.exports = chunkArray
+// Using while loop
+// function chunkArray(array, size) {
+//   let result = [];
+//   let arrayCopy = [...array];
+//   while (arrayCopy.length > 0) {
+//     result.push(arrayCopy.splice(0, size));
+//   }
+//   return result;
+// }
+
+// function chunkArray(array, size) {
+//   let result = []
+//   for (let i = 0; i < array.length; i += size) {
+//     let chunk = array.slice(i, i + size)
+//     result.push(chunk)
+//   }
+//   return result
+// }
+
+// Recursive approach
+// function chunkArray(array, size) {
+//   if (array.length <= size) {
+//     return [array]
+//   }
+//   return [array.slice(0, size), ...chunkArray(array.slice(size), size)]
+// }
+
+module.exports = chunkArray;
